6a2621c60d5a6c0315a22421b94ca6dcca6845a8
liz-gunther <84788972+liz-gunther@users.noreply.github.com>
2022-04-25 12:56:51 -0500

    updated readme

--- src/main/java/com/lizgunther/videouniverse/security/UserDetailsRepository.java
+++ src/main/java/com/lizgunther/videouniverse/security/UserDetailsRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserDetailsRepository extends JpaRepository<User, Long> {
 
-
     User findUserById(Long Id);
 
     User findByEmail(String email);
